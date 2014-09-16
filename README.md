8a577772b598b5e840224965afc9df2cbc2f3547
Jim Allman <jim@ibang.com>
2014-09-16 16:35:54 -0400

    Adding interactive view of each tree

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ phylostylotastic
 ================
 
 Styled publication of NeXML/NeXSON trees
+
+**Prerequisites**: Python 2.5, [ete2](https://pypi.python.org/pypi/ete2/) module installed.
--- pstastic.py
+++ pstastic.py
@@ -8,8 +8,11 @@ if len(sys.argv) < 2:
 nexml = Nexml()
 nexml.build_from_file(sys.argv[1])
 
+# render a series of SVG files (one for each tree)
 for trees in nexml.get_trees():
     tree_index = 0
     for tree in trees.get_tree():
         tree_index += 1
         tree.render("output%d.svg" % tree_index)
+	# let's try the interactive QT viewer
+        tree.show()
