b4dceeaf6c5f645ee887275036a3470aa9c5145a
Jim Allman <jim@ibang.com>
2014-09-16 16:51:45 -0400

    Sketch in TSS gathering

--- pstastic.py
+++ pstastic.py
@@ -1,4 +1,4 @@
-from ete2 import Nexml
+from ete2 import Nexml, TreeStyle
 import sys
 
 if len(sys.argv) < 2:
@@ -8,11 +8,27 @@ if len(sys.argv) < 2:
 nexml = Nexml()
 nexml.build_from_file(sys.argv[1])
 
+def build_tree_style(tree):
+    # use our simple TSS cascade to prepare an ETE TreeStyle object
+    sheets = gather_tss_stylesheets(tree)
+    if len(sheets) == 0:
+        return None
+    for s in sheets:
+        ts = TreeStyle()
+        return ts
+
+def gather_tss_stylesheets(tree):
+    return []
+
 # render a series of SVG files (one for each tree)
 for trees in nexml.get_trees():
     tree_index = 0
     for tree in trees.get_tree():
         tree_index += 1
+        tree_style = build_tree_style(tree)
         tree.render("output%d.svg" % tree_index)
-	# let's try the interactive QT viewer
+        # let's try the interactive QT viewer
         tree.show()
+
+
+
