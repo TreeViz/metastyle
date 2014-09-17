ad0d230ffb322eee9d931dfb6b267163ff81ee95
Jim Allman <jim@ibang.com>
2014-09-16 23:13:20 -0400

    Adding per-node hook for TSS styling

--- pstastic.py
+++ pstastic.py
@@ -17,18 +17,50 @@ nexml.build_from_file(sys.argv[1])
 
 def build_tree_style(tree):
     # use our simple TSS cascade to prepare an ETE TreeStyle object
-    ##import pdb; pdb.set_trace()
     sheets = gather_tss_stylesheets(tree)
     if len(sheets) == 0:
         return None
+
+    # Some styles can be applied to the entire tree
     ts = TreeStyle()
+    # For nodes (and other elements?), build an ordered set of TSS rules 
+    # to apply to each element in our layout function
+    node_rules = []
+
     for s in sheets:
-        ts = apply_stylesheet(stylesheet=s, tree_style=ts)
+        ts, tss_cascade = apply_stylesheet(
+            stylesheet=s, 
+            tree_style=ts,
+            node_rules=node_rules)
 
-    # TODO: do this again for NodeStyle objects, applied to node subsets?
-    # OR apply a layout function to test each node against TSS selectors?
+    # Use a layout function to test each node against TSS selectors?
+    def apply_tss(node):
+        node_style = NodeStyle()
+        for rule in node_rules:
+            # Test this node against each selector
+            if test_node_against_selector(node, rule.selector):
+                node_style, node = apply_node_rule(rule, node_style, node)
+        node.set_style(node_style);
+        return node
 
+    # apply this layout function to each node as it's rendered
+    ts.layout_fn = apply_tss
     return ts
+
+def test_node_against_selector(node, selector):
+    # TODO: Here we can interpret our selectors 
+    # in the context of a live tree (vs. a DOM)
+    return True
+
+def apply_node_rule(rule, node_style, node):
+    for style in rule.declarations:
+        print(style)
+    node_style["shape"] = "sphere"
+    node_style["fgcolor"] = "orange"
+    node_style["hz_line_type"] = 2
+    node_style["hz_line_color"] = "#cc99cc"
+    node_style["size"] = 10
+    return node_style, node
 
 def gather_tss_stylesheets(tree):
     sheets = []
@@ -53,13 +85,13 @@ def gather_tss_stylesheets(tree):
     return sheets 
 
 # Apply styles to an existing TreeStyle object and return it
-def apply_stylesheet(stylesheet, tree_style):
+def apply_stylesheet(stylesheet, tree_style, node_rules):
     if (not stylesheet):
         print("Missing stylesheet!")
-        return tree_style
+        return tree_style, node_rules
     if (not tree_style):
         print("Missing tree_style!")
-        return None
+        return None, None
 
     # parse the TSS from its CSS-style syntax
     parser = tinycss.make_parser('page3')
@@ -73,6 +105,7 @@ def apply_stylesheet(stylesheet, tree_style):
             print(e)
 
     # walk the TSS rules and translate them into TreeStyle properties
+    # or add them to the node_rules collection
     for r in style.rules:
         if r.at_keyword:
             # this is an ImportRule, MediaRule, or the like
@@ -81,21 +114,23 @@ def apply_stylesheet(stylesheet, tree_style):
             print(r)
         else:
             # it's a normal CSS RuleSet
-            print("TSS RuleSet:")
-            print("  selector: %s" % r.selector.as_css())
-            print("  as list:  %s" % repr(r.selector))
-            print("  declarations: %s" % r.declarations)
+            #print("TSS RuleSet:")
+            #print("  selector: %s" % r.selector.as_css())
+            #print("  as list:  %s" % repr(r.selector))
+            #print("  declarations: %s" % r.declarations)
 
-            # TODO: interpret each selector to find targets
+            # add every rule to node tests 
+            node_rules.append(r)
+
+            # TODO: interpret its selector to find targets
             # see https://pythonhosted.org/tinycss/parsing.html
 
             # TODO: modify the current TreeStyle to reflect its declarations
-            # TODO: modify n NodeStyle objects and apply them..?
-
             #tree_style.mode = 'c'  # circular
             #tree_style.show_leaf_name = False
             #tree_style.show_branch_length = True
-    return tree_style
+
+    return tree_style, node_rules
 
 # render a series of SVG files (one for each tree)
 for trees in nexml.get_trees():
@@ -104,18 +139,6 @@ for trees in nexml.get_trees():
         tree_index += 1
         ts = build_tree_style(tree)
         tree.render("output%d.svg" % tree_index, tree_style=ts)
-
-        # TEMP demo of a NodeStyle object applied to a few nodes
-        node_size = 2
-        for n in tree.traverse():
-            ns = NodeStyle()
-            ns["shape"] = "sphere"
-            ns["fgcolor"] = "darkred"
-            ns["hz_line_type"] = 1
-            ns["hz_line_color"] = "#cccccc"
-            ns["size"] = node_size
-            node_size += 2
-            n.set_style(ns);
 
         # let's try the interactive QT viewer
         tree.show(tree_style=ts)
