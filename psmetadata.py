a5d0c18b2c13db01749197e5defc59f279356f5c
gaurav <gaurav@ggvaidya.com>
2014-09-18 18:32:22 -0400

    Saving changes to psmetadata.

--- psmetadata.py
+++ psmetadata.py
@@ -1,10 +1,10 @@
 #
-# Displays all the metadata contained in a file that PhyloStyloTastic can
-# see or render.
+# Displays all the metadata contained in a NeXML file.
 # 
 
-import ete2
+import dendropy
 from pprint import pprint
+from StringIO import StringIO
 import argparse
 import sys
 import os
@@ -20,27 +20,64 @@ argparser.add_argument(
 args = argparser.parse_args()
 
 # Load NeXML files.
-nexml = ete2.Nexml()
-nexml.build_from_file(args.source[0])
+ds = dendropy.DataSet.get_from_path(args.source[0], 'nexml', attach_taxon_set=True)
 
-# Go through the entire DOM, looking for Meta objects, and display them.
-def displayMeta(parent, meta):
-    print("parent = " + str(parent) + ", meta = " + str(meta));
+#
+# SECTION 1: Look for Meta nodes (http://www.nexml.org/nexml/html/doc/schema-1/meta/annotations/#Meta)
+# and display them.
+#
+metadata_order = []
+metadata = dict()
 
-def recurse(node):
-    try:
-        for meta in node.get_meta():
-            displayMeta(node, meta)
-    except AttributeError:
-        pass
+def search_for_meta_nodes(node):
+    # Check for meta elements and display them.
+    for annotation in node.annotations:
+        node_id = str(node.__class__.__name__) + " " + node.oid
 
-    try:
-        for child in node:
-            recurse(child)
-    except TypeError:
-        pass
+        if node_id in metadata:
+            metadata[node_id].append(annotation)
+        else:
+            metadata_order.append(node_id)
+            metadata[node_id] = [annotation]
 
-recurse(nexml)
-for trees in nexml.get_trees():
-    for tree in trees.get_tree():
-        recurse(tree)
+# Search for meta nodes on:
+# - The top-level Nexml node.
+search_for_meta_nodes(ds)
+
+# - Each OTU.
+for taxonsets in ds.taxon_sets:
+    for taxon in taxonsets:
+        search_for_meta_nodes(taxon)
+
+# - Each trees and tree node.
+for tree_list in ds.tree_lists:
+    for tree in tree_list:
+        search_for_meta_nodes(tree)
+
+# Display everything.
+property_names = []
+property_values = dict()
+property_types = dict()
+
+def record_property(name, value, proptype):
+    property_names.append(name)
+
+    if name not in property_values:
+        property_values[name] = dict()
+    if value not in property_values[name]:
+        property_values[name][value] = 0
+    property_values[name][value] += 1
+
+    if name not in property_types:
+        property_types[name] = dict()
+    if value not in property_types[name]:
+        property_types[name][value] = 0
+    property_types[name][value] += 1
+
+for node_id in metadata_order:
+    print(" - " + node_id)
+    for meta in metadata[node_id]:
+        record_property(meta.name, meta.value, meta.datatype_hint)
+        print("    - %s: %s (%s)" % (meta.name, meta.value, meta.datatype_hint))
+
+pprint(property_values)
