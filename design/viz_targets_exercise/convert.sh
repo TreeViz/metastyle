6e6bb419f1e6a28b92aad0f86c77413951759d65
Rutger Vos <rutgeraldo@gmail.com>
2016-11-29 23:47:12 +0100

    Update convert.sh

--- design/viz_targets_exercise/convert.sh
+++ design/viz_targets_exercise/convert.sh
@@ -2,6 +2,8 @@
 # driver script to explain the command line arguments
 # This requires:
 # from CPAN, XML::Twig, and github.com/rvosa/bio-phylo
+# note also the pipe through xml_pp, one of the optional
+# scripts from XML::Twig that you should opt to install
 perl convert.pl \
 	-verbose \
 	-tips tip_data.csv,tip_data_meta.csv \
